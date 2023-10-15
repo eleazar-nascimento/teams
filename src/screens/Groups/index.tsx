f7b40d6d7fb3f962694adb97a91e11dd457d91ae
Eleazar Nascimento <eleazar.nascimento@gmail.com>
2023-10-15 19:36:06 -0300

    feat: implements groups list

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -2,8 +2,15 @@ import { Header } from '@components/Header';
 import { Container } from './styles';
 import { Highlight } from '@components/Highlight';
 import { GroupCard } from '@components/GroupCard';
+import { useState } from 'react';
+import { FlatList } from 'react-native';
 
 export function Groups() {
+  const [groups, setGroups] = useState<Array<string>>([
+    'Galera do Quiquito',
+    'Devs Unidos'
+  ]);
+
   return (
     <Container>
      <Header />
@@ -13,9 +20,16 @@ export function Groups() {
       subtitle='jogue com a sua turma'
      />
 
-     <GroupCard
-      title='Devs Unidos'
+    <FlatList 
+      data={groups}
+      keyExtractor={item => item}
+      renderItem={({ item }) => (
+        <GroupCard
+          title={item}
       />
+      )}
+    />
+
     </Container>
   );
 }
