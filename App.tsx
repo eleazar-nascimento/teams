40b5233209c292a2ba479d39a95aed682d6d3c13
Eleazar Nascimento <eleazar.nascimento@gmail.com>
2023-10-15 18:08:54 -0300

    feat: create loading component

--- App.tsx
+++ App.tsx
@@ -1,9 +1,9 @@
-import { ActivityIndicator } from 'react-native';
 import { ThemeProvider } from 'styled-components';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
 import { Groups } from './src/screens/Groups';
 import theme from '@theme/index';
+import { Loading } from '@components/Loading';
 
 export default function App() {
   const [fontsLoaded, ] = useFonts({
@@ -18,8 +18,8 @@ export default function App() {
   return (
     <ThemeProvider theme={theme} >
       { 
-        fontsLoaded ?
-        <Groups /> : <ActivityIndicator />
+        !fontsLoaded ?
+        <Groups /> : <Loading />
       }
     </ThemeProvider>
   );
