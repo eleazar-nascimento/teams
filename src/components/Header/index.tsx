5ca9954b0b6943e63348f6c3b957af2b6db5d127
Eleazar Nascimento <eleazar.nascimento@gmail.com>
2023-10-15 18:46:02 -0300

    chore: implements icons

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,23 @@
-import { Container, Logo } from "./styles";
+import { BackButton, BackIcon, Container, Logo } from "./styles";
 
 import logoImg from '@assets/logo.png';
 
-export function Header() {
-    return (
-        <Container>
-            <Logo source={logoImg} />
-        </Container>
-    );
+type HeaderProps = {
+    showBackButton?: boolean;
+}
+
+export function Header({
+    showBackButton = false
+}: HeaderProps) {
+  return (
+      <Container>
+        {
+          showBackButton &&
+            <BackButton>
+              <BackIcon />
+            </BackButton>   
+        }
+      <Logo source={logoImg} />
+    </Container>
+  );
 }
