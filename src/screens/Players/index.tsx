35815dce6fe00cef5b21747a82a6878f9a54ff26
Eleazar Nascimento <eleazar.nascimento@gmail.com>
2023-10-31 23:41:26 -0300

    feat: create ButtonIcon component

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,16 +1,25 @@
 import { Header } from "@components/Header";
 import { Container } from "./styles";
 import { Highlight } from "@components/Highlight";
+import { ButtonIcon } from "@components/ButtonIcon";
+import { Input } from "@components/Input";
+import { Form } from "@components/ButtonIcon/styles";
 
 export function Players() {
-	return(
-		<Container>
-			<Header showBackButton />
+  return (
+    <Container>
+      <Header showBackButton />
 
-			<Highlight 
-				title="Nome da Turma"
-				subtitle="adicione a galera e separe os times"
-			/>
-		</Container>
-	);
+      <Highlight
+        title="Nome da Turma"
+        subtitle="adicione a galera e separe os times"
+      />
+
+      <Form>
+        <Input placeholder="Nome da pessoa" autoCorrect={false} />
+
+        <ButtonIcon icon="add" />
+      </Form>
+    </Container>
+  );
 }
